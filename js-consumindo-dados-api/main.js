cacaa853cb8a9d8faa390d4250ba1b43cb1e1d66
Wesley Paiva <wesleypaiva943@gmail.com>
2023-05-12 23:08:25 -0300

    aplicação finalizada e testada

--- js-consumindo-dados-api/main.js
+++ js-consumindo-dados-api/main.js
@@ -20,25 +20,43 @@ let consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
 
 async function buscaEndereco(cep) {
 
+    let mensagemErro = document.getElementById('erro');
+    //com o innerHTML conseguimos alterar diretamente o html 
+    mensagemErro.innerHTML = "";
+
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        var consultaCEPConvertida = await consultaCEP.json()
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        let consultaCEPConvertida = await consultaCEP.json()
 
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!')
         }
 
+        // pegar os dados do objeto, através do getElementById
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('endereco');
+        let estado = document.getElementById('estado');
+        let bairro = document.getElementById('bairro')
+
+        // acesso o valor do objeto, e altero seu valor, utilizando o .value, atribuindo o item específico deste objeto após tratado.
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
         
     }   catch (erro) {
+        mensagemErro.innerHTML =  `<p>CEP inválido. Tente novamente!</p>`
         console.log(erro)
     }
     
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focosout", () => buscaEndereco(cep, value));
+//estas duas linhas de código abaixo pegam o campo CEP e ativam a função após clicar em outra parte para tirar o foco, com o "focusout"
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
 
 
