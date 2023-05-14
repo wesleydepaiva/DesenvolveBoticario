abd80a8aac6c4ced85bddcdb723c385c1350e996
Wesley de Paiva <wesleypaiva943@gmail.com>
2023-05-13 21:49:39 -0300

    função editar adicionada

--- 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
+++ 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
@@ -32,10 +32,36 @@ const removeCliente = (id) => {
     })
 }
 
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then( resposta => {
+        return resposta.json()
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        }) 
+    })
+    .then( resposta => {
+        return resposta.json()
+    })
+
+}
+
 //funções exportadas de uma vez só através dessa const
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 
 }
